4c02bc140cdffe1b87e74a6d38cc019b9c06eeed
Devin <946588265@qq.com>
2019-12-08 16:17:46 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/devin/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.devin.git;
+/*
+我的git测试
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
